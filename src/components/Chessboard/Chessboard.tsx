fb8d18286fc2d529f51b2935a0c4705af7cbb244
Yaguera <ya-gotre@hotmail.com>
2024-10-11 15:55:23 -0300

    feat: prevent the pieces to get drag out of the board

--- src/components/Chessboard/Chessboard.tsx
+++ src/components/Chessboard/Chessboard.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useRef, useState } from 'react';
 import { pieces} from '../../constants/pieces';
 import Square from '../Square/Square';
 import './Chessboard.css'
@@ -8,6 +8,8 @@ const horizontalAxis:string[] =["a","b","c","d","e","f","g","h"];
 
 
 const Chessboard = () => {
+    const chessboardRef = useRef<HTMLDivElement>(null)
+
     const [activePiece, setActivePiece] = useState<HTMLElement | null>(null);
     const board = [];
 
@@ -45,13 +47,21 @@ const Chessboard = () => {
     }
 
     const movePiece = (e:React.MouseEvent) => {
-        if(activePiece){
+        const chessboard = chessboardRef.current;
+        if(activePiece && chessboard){
+            const minX = chessboard.offsetLeft - 30
+            const minY = chessboard.offsetTop - 30
+            const maxX = chessboard.offsetLeft + chessboard.clientWidth - 30
+            const maxY = chessboard.offsetTop + chessboard.clientHeight - 30
             const x = e.clientX - 31.25;
             const y = e.clientY - 31.25;
-
             activePiece.style.position = "absolute";
-            activePiece.style.left = `${x}px`
-            activePiece.style.top = `${y}px`
+            // activePiece.style.left = `${x}px`
+            // activePiece.style.top = `${y}px`
+
+           activePiece.style.left = x < minX ? `${minX}px` : x > maxX ? `${maxX}px`:`${x}px`
+           activePiece.style.top = y < minY ? `${minY}px` : y > maxY ? `${maxY}px`:`${y}px`
+   
         }
     }
 
@@ -61,7 +71,15 @@ const Chessboard = () => {
     
     return (
     <div>
-        <div onMouseMove={movePiece} onMouseDown={grabPiece} onMouseUp={dropPiece} id="chessboard">{board}</div>
+        <div 
+            onMouseMove={movePiece} 
+            onMouseDown={grabPiece} 
+            onMouseUp={dropPiece} 
+            id="chessboard"
+            ref={chessboardRef}
+        >
+                {board}
+        </div>
     </div>
     )
     }
